7dd71afaddadbd8657effad18d826a4b3436729c
Ryusuke Ihashi <72903264+Akutagawa50@users.noreply.github.com>
2022-10-15 13:41:57 +0900

    Update Test_AnalogIn.ino

--- Test_AnalogIn/Test_AnalogIn.ino
+++ Test_AnalogIn/Test_AnalogIn.ino
@@ -1,18 +1,18 @@
 #define LOW_PASS_FILTER(x0, x1) ((double)(x0)*0.85+(double)(x1)*0.15)
-#define ANALOG_INPUT 13
+#define ARRAY_LEN(x) (sizeof((x))/sizeof((x)[0]))
 
-int buff = 0;
+int pins[]={13};
+int buff[ARRAY_LEN(pins)] = {0};
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  int s1 = analogRead(ANALOG_INPUT);
-  buff = LOW_PASS_FILTER(buff,s1);
-  //Serial.printf("S1: %4d, Vin: %6.4f\n\r", s1, (double)s1 * 3.3 / 4092.0, 3);
-  Serial.println(buff);//LPFを通したADコンバータの値
-  //Serial.println((double)s1 * 3.3 / 4092.0);//電圧
-  //Serial.println(s1);//ADコンバータの値
+  for(int i=0; i<ARRAY_LEN(pins); i++){
+    int s = analogRead(pins[i]);
+    buff[i] = LOW_PASS_FILTER(buff[i],s);
+    Serial.println(buff[i]);//LPFを通したADコンバータの値
+  }
   delay(100);
 }
